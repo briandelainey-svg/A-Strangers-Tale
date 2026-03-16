641055668a5cdabebee5e501bccf509d20f83b41
briandelainey-svg <brian.delainey@lskysd.ca>
2026-03-16 09:52:12 -0600

    Add files via upload

--- Ext_Fil_SumBAD.py
+++ Ext_Fil_SumBAD.py
@@ -62,7 +62,9 @@ classes = {
     }
 }#Classes and Subclasses
 
-inventory = {}#Current Held Items
+inventory = {'Weapon': 0,
+             'Armor': 0
+             }#Current Held Items
 
 stats = {
     'Str': 5,
@@ -93,6 +95,8 @@ def cabin(weapon, choices, state):
 3. Leave the Cabin''')
         choice = input('''>>> ''')
         choices.append(choice)
+        if choice  == 'b':
+            print(inventory)
         if choice == '1':
             slow_print('You curl up in bed for a short rest')
             print('You recover all your health.')
@@ -107,12 +111,12 @@ def cabin(weapon, choices, state):
                 investigate += 1
         elif choice == '3':
             state = 1
-            break
+            return
         else:
             fail()
 
 def cabin_ext(choices, well, style):
-    slow_print('Standing outside the cabin, you see a dense forest surounding you, with only a small deer path leeding outwards.')
+    slow_print('Standing outside your home, you see a dense forest surounding you, with only a small deer path leeding outwards.')
     while True:
         choice = input('''What do you do?
 1. Go inside
@@ -120,9 +124,11 @@ def cabin_ext(choices, well, style):
 3. Follow the trail
 >>> ''')
         choices.append(choice)
+        if choice  == 'b':
+            print(inventory)
         if choice == '1':
             state = 0
-            break
+            return state
         elif choice == '2':
             slow_print('Walking around the cabin, you find a mostly empty yard. The only items of note being a deer skull staring through the trees,')
             slow_print('always following your gaze, and an old well, coated in blood.')
@@ -133,8 +139,22 @@ def cabin_ext(choices, well, style):
 3. Go to the front of the Cabin
 >>> ''')
                 choices.append(choice)
+                if choice  == 'b':
+                    print(inventory)
                 if choice == '1':
                     slow_print('Looking through the woods, you find the deer skull is always 2 trees away. Always at the edge of your vision.')
+                    while True:
+                        choice = input('''Do you continue, or return?
+1.Continue onward
+2.Return to the well
+>>> ''')
+                        choices.append(choice)
+                        if choice  == 'b':
+                            print(inventory)
+                        if choice == '1':
+                            slow_print('')
+                        elif choice == '2':
+                            break
                 elif choice == '2':
                     if well == False:
                         well = True
@@ -158,9 +178,10 @@ def cabin_ext(choices, well, style):
                     fail()
         elif choice == '3':
             state = 2
-            break
+            return state
         else:
             fail()
+        
             
 def town():
     slow_print('You enter town square, with the [Butcher], the [Blacksmith], and the [Library]')
@@ -170,6 +191,7 @@ def woods(Dragon):
         slow_print("You can't seem to enter the woods, as if some unseen force is preventing your movements")
         slow_print(f'"Come back when you are stronger, Hero!"')
         state = 2
+        
     elif Dragon == True:
         slow_print('The woods that once excluded you entry now let you pass.')
 
@@ -182,12 +204,12 @@ def level(exp, level):
         exp = 0
         level += 1
         lvlup = input('''What stat do you wish to increase?
-1.Strength
-2.Dexterity
-3.Constitution
-4.Intelligence
-5.Wisdom
-6.Charisma
+Str(Strength)
+Dex(Dexterity)
+Com(Constitution)
+Int(Intelligence)
+Wis(Wisdom)
+Lck(Charisma)
 >>> ''')
         choices.append(choice)
         if lvlup == '1' or lvlup == 'Strength':
@@ -205,9 +227,9 @@ def level(exp, level):
         elif lvlup == '5' or lvlup == 'Wisdom':
             print('Wisdom has Been increased by 3!')
             stats['Wis'] += 3
-        elif lvlup == '6' or lvlup == 'Charisma':
-            print('Charisma has Been increased by 3!')
-            stats['Cha'] += 3
+        elif lvlup == '6' or lvlup == 'Luck':
+            print('Luck has Been increased by 3!')
+            stats['Lck'] += 3
     else:
         tnl = 100 - exp
         slow_print(f'You need {tnl} more experience until next level up.')
@@ -231,9 +253,9 @@ if name == 0:
     slow_print(f'So, {name}. How do you perfer to fight?')
     time.sleep(.5)
     style = input('''
-1. With a Sword(Str and Dex)
+1. With a Sword(Str and Con)
 2. With Staff and Spell(Int and Wis)
-3. With my Fists(Str and Con)
+3. With my Fists(Dex and Lck)
 >>> ''')
     choices.append(choice)
     if style == '1':
@@ -244,19 +266,20 @@ if name == 0:
         weapon = '[Staff]'
     elif style == '3':
         slow_print('Daring are we, [Brawler]?')
-        weapon = 'set of [Fist Bindings]'
+        weapon = '[Fist Binding]'
     slow_print('You awaken at what seems to be home, yet remain unaware of where you are')
-    slow_print(f'Next to the door you see a {weapon} hanging from a coat stand.')
-    slow_print(f'You equip the {weapon} and leave the Cabin.')
+    slow_print(f'Next to the door you see a {weapon} and a backpack hanging from a coat stand.')
+    slow_print(f'You equip the {weapon} and bag as you leave the Cabin.')
     inventory ['Weapon'] = weapon
     state = 1
-if state == 0:#Cabin Interior
-    cabin(weapon, choices)
-elif state == 1:#Cabin Exterior
-    cabin_ext(choices, well, style)
-elif state == 2:#Town
-    town(choices)
-elif state == 3:#Wandering Woods
-    woods(choices)
-elif state == 4:#Castle
-    castle(choices)
+while True:
+    if state == 0:#Cabin Interior
+        cabin(weapon, choices)
+    elif state == 1:#Cabin Exterior
+        cabin_ext(choices, well, style)
+    elif state == 2:#Town
+        town(choices)
+    elif state == 3:#Wandering Woods
+        woods(choices)
+    elif state == 4:#Castle
+        castle(choices)
